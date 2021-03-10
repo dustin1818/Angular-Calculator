d368c77be8188605fc75e08a93444f8016af1109
dustin1818 <40887709+dustin1818@users.noreply.github.com>
2021-03-10 23:53:57 +0800

    adding tooltip component

--- app.module.ts
+++ app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CalculatorComponent } from './calculator/calculator.component';
 import { ContainerComponent } from './container/container.component';
+import { TooltipComponent } from './tooltip/tooltip.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CalculatorComponent,
-    ContainerComponent
+    ContainerComponent,
+    TooltipComponent
   ],
   imports: [
     BrowserModule,
